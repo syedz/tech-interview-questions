af725290a916ab6ff1e35ebb674f55829aa20b84
Zeeshan Syed <zsyed89@gmail.com>
2019-01-27 22:11:20 -0500

    Added code for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Solution 1 - Had to draw this out to visually see rows and cols
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let col = 0; col < n; col++) {
+//             if (col <= row) {
+//                 stair += '#';
+//             } else if (col > row) {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+// Solution 2 - Go through this solution after it's implemented
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' ';
+
+    steps(n, row, stair + add);
+}
 
 module.exports = steps;
